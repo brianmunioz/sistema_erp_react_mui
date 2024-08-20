3a0eb5e1be5e5e2aecdc8513276c167e4c7c9239
brianmunioz <brianmunozjob@gmail.com>
2024-08-20 17:22:49 -0300

    FIX: Arreglar error de que al hacer scroll se ve fondo gris y colores de letras en estadisticas

--- src/components/home/EstadisticasComp.jsx
+++ src/components/home/EstadisticasComp.jsx
@@ -18,22 +18,22 @@ const EstadisticasComp = () => {
   return (
     <Box p={5}>
     <Typography color="primary" fontWeight={800}> Inventario</Typography>
-    <Typography fontWeight={800}>Ganancia estimada:<span style={{color: colorSpan}}> ${inventario.reduce((acumulador, actual) => acumulador + ((actual.precioVenta - actual.precioCompra)*actual.cantidad), 0).toFixed(2)}</span></Typography>
-    <Typography fontWeight={800}>Costo: <span style={{color: colorSpan}}>${inventario.reduce((acumulador, actual) => acumulador + ((actual.precioCompra)*actual.cantidad), 0).toFixed(2)} </span></Typography>
-    <Typography fontWeight={800}>Total a vender a precio venta:<span style={{color: colorSpan}}> ${inventario.reduce((acumulador, actual) => acumulador + ((actual.precioVenta)*actual.cantidad), 0).toFixed(2)}</span></Typography>
+    <Typography sx={{color: "grey"}} fontWeight={800}>Ganancia estimada:<span style={{color: colorSpan}}> ${inventario.reduce((acumulador, actual) => acumulador + ((actual.precioVenta - actual.precioCompra)*actual.cantidad), 0).toFixed(2)}</span></Typography>
+    <Typography sx={{color: "grey"}} fontWeight={800}>Costo: <span style={{color: colorSpan}}>${inventario.reduce((acumulador, actual) => acumulador + ((actual.precioCompra)*actual.cantidad), 0).toFixed(2)} </span></Typography>
+    <Typography sx={{color: "grey"}} fontWeight={800}>Total a vender a precio venta:<span style={{color: colorSpan}}> ${inventario.reduce((acumulador, actual) => acumulador + ((actual.precioVenta)*actual.cantidad), 0).toFixed(2)}</span></Typography>
 
-    <Typography fontWeight={800}>Cantidad total: <span style={{color: colorSpan}}>{inventario.reduce((acumulador, actual) => acumulador + parseInt(actual.cantidad), 0)}</span></Typography>
-    <Typography color="primary" fontWeight={800}> Ventas</Typography>
+    <Typography sx={{color: "grey"}} fontWeight={800}>Cantidad total: <span style={{color: colorSpan}}>{inventario.reduce((acumulador, actual) => acumulador + parseInt(actual.cantidad), 0)}</span></Typography>
+    <Typography  color="primary" fontWeight={800}> Ventas</Typography>
 
-    <Typography fontWeight={800}>Ganancia de productos vendidos: <span style={{color: colorSpan}}>${inventarioVendido.reduce((acumulador, actual) => acumulador + ((actual.precioVenta - actual.precioCompra)*actual.cantidad), 0).toFixed(2)}</span></Typography>
-    <Typography fontWeight={800}>Total vendido (precio de venta): <span style={{color: colorSpan}}>${inventarioVendido.reduce((acumulador, actual) => acumulador + ((actual.precioVenta)*actual.cantidad), 0).toFixed(2)}</span></Typography>
-    <Typography color="primary" fontWeight={800}> Ganancias obtenidas por categoría</Typography>
+    <Typography sx={{color: "grey"}} fontWeight={800}>Ganancia de productos vendidos: <span style={{color: colorSpan}}>${inventarioVendido.reduce((acumulador, actual) => acumulador + ((actual.precioVenta - actual.precioCompra)*actual.cantidad), 0).toFixed(2)}</span></Typography>
+    <Typography sx={{color: "grey"}} fontWeight={800}>Total vendido (precio de venta): <span style={{color: colorSpan}}>${inventarioVendido.reduce((acumulador, actual) => acumulador + ((actual.precioVenta)*actual.cantidad), 0).toFixed(2)}</span></Typography>
+    <Typography  color="primary" fontWeight={800}> Ganancias obtenidas por categoría</Typography>
     {
-      categoria.map(cat=><Typography fontWeight={800}>Total de categoría {cat}: <span style={{color: colorSpan}}>${inventarioVendido.filter(e=>e.categoria === cat).reduce((acumulador, actual) => acumulador + ((actual.precioVenta-actual.precioCompra)*actual.cantidad), 0).toFixed(2)}</span></Typography>)
+      categoria.map(cat=><Typography sx={{color: "grey"}} fontWeight={800}>Total de categoría {cat}: <span style={{color: colorSpan}}>${inventarioVendido.filter(e=>e.categoria === cat).reduce((acumulador, actual) => acumulador + ((actual.precioVenta-actual.precioCompra)*actual.cantidad), 0).toFixed(2)}</span></Typography>)
     }
     <Typography color="primary" fontWeight={800}> Ganancias obtenidas por producto</Typography>
     {
-      inventario.map(cat=><Typography fontWeight={800}>Total de {cat.producto}: <span style={{color: colorSpan}}>${inventarioVendido.filter(e=>e.producto === cat.producto).reduce((acumulador, actual) => acumulador + ((actual.precioVenta-actual.precioCompra)*actual.cantidad), 0).toFixed(2)}</span></Typography>)
+      inventario.map(cat=><Typography sx={{color: "grey"}}fontWeight={800}>Total de {cat.producto}: <span style={{color: colorSpan}}>${inventarioVendido.filter(e=>e.producto === cat.producto).reduce((acumulador, actual) => acumulador + ((actual.precioVenta-actual.precioCompra)*actual.cantidad), 0).toFixed(2)}</span></Typography>)
     }
     
 
