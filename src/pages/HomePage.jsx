99b0b8517187c21bb68f503eba45f961772ca42e
brianmunioz <brianmunozjob@gmail.com>
2024-08-22 15:48:21 -0300

    AGREGAR: powered by argenboost

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Button, Stack } from '@mui/material'
+import { Button, Link, Stack, Typography } from '@mui/material'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Colors } from '../utils/Colors';
@@ -9,6 +9,19 @@ const HomePage = () => {
     <Stack direction={"column"} width="100vw" height="100vh" backgroundColor={Colors.cuaternary.main} justifyContent={"center"} alignItems={"center"}>
         <img src="/logo.png" width={"200px"} height={"200px"}/>
         <Button onClick={()=>navigate('/productos')} variant="contained" color='primary'sx={{color:"#fff", fontWeight: "bold"}}>Entrar</Button>
+       <Link 
+       href="https://argenboost.com.ar" 
+       mt={5} 
+       height={"20px"} 
+       sx={{
+        cursor: "pointer",
+        "&:hover": {
+      color: "#371B58" 
+    }}} 
+    underline="none" 
+    target="_blank" 
+    fontWeight={800} 
+    color="#371B58">Powered by <img src='/argenboost.svg' style={{position: "relative", top: "16px"}} width={"40px"} height={"40px"}/> Argenboost</Link>
     </Stack>
   )
 }
